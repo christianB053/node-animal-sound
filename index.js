730f8e1f883b2af553c2fe37c48e080dfc1376f5
hb_kim <83122749+christianB053@users.noreply.github.com>
2022-08-14 20:13:57 +0900

    index.js json->img 수정

--- index.js
+++ index.js
@@ -14,13 +14,29 @@ app.get("/sound/:name", (req, res) => {
   console.log(name);
 
   if (name == "dog") {
-    res.json({ sound: "멍멍" });
+    res.send(
+      "<img src='https://cdn.pixabay.com/photo/2019/08/19/07/45/corgi-4415649_960_720.jpg'>"
+    );
   } else if (name == "cat") {
-    res.json({ sound: "야옹" });
+    res.send(
+      "<img src='https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_960_720.jpg'>"
+    );
   } else if (name == "pig") {
-    res.json({ sound: "꿀꿀" });
+    res.send(
+      "<img src='https://cdn.pixabay.com/photo/2013/08/12/18/10/staniel-cay-171908_960_720.jpg'>"
+    );
+  } else if (name == "duck") {
+    res.send(
+      "<img src='https://cdn.pixabay.com/photo/2018/07/08/18/09/mallard-3524390_960_720.jpg'>"
+    );
+  } else if (name == "rabbit") {
+    res.send(
+      "<img src='https://cdn.pixabay.com/photo/2017/04/02/22/36/easter-2197043_960_720.jpg'>"
+    );
   } else {
-    res.json({ sound: "알수없음" });
+    res.send(
+      "<img src='https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_960_720.jpg'>"
+    );
   }
 });
 
